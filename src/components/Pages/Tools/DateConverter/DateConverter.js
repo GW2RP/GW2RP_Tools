f7cc5dd2eb15fd577d8ebba10d4fa2afd9decaed
THIZY Kevin <Nakasar@users.noreply.github.com>
2018-08-18 01:31:52 +0200

    Date Conversion without calling the API.

--- src/components/Pages/Tools/DateConverter/DateConverter.js
+++ src/components/Pages/Tools/DateConverter/DateConverter.js
@@ -27,9 +27,9 @@ class DateConverter extends Component {
     convertDateToMouvelian = (event) => {
         event.preventDefault();
 
-        let year, month, day;
+        let year, month, day, date;
         try {
-            const date = new Date(event.target.value);
+            date = new Date(event.target.value);
 
             year = date.getFullYear();
             month = date.getMonth() + 1;
@@ -38,8 +38,45 @@ class DateConverter extends Component {
             console.error('Could not parse date.');
         }
 
-        Axios.get(`https://api.nakasar.me/convertir/rp/${day}/${month}/${year}`).then(response => {
-            const date = response.data.day.date;
+        Promise.resolve(date).then(toConvert => {
+            const day = Math.ceil((toConvert - new Date(toConvert.getFullYear(), 0, 1)) / 86400000);
+    
+            const bisex = toConvert.getFullYear() % 4 === 0;
+            const offset = bisex ? 1 : 0;
+    
+            const date = {
+                irl: toConvert,
+                day_of_year: day,
+                year: toConvert.getFullYear() - 687,
+            };
+            if (day < 90 + offset) {
+                date.day = day;
+                date.season = {
+                    code: 0,
+                    name: 'Zéphyr'
+                };
+            } else if (day < 180 + offset) {
+                date.day = day - (90 + offset);
+                date.season = {
+                    code: 1,
+                    name: 'Phénix'
+                };
+            } else if (day < 270 + offset) {
+                date.day = day - (180 + offset);
+                date.season = {
+                    code: 2,
+                    name: 'Scion'
+                };
+            } else {
+                date.day = day - (270 + offset);
+                date.season = {
+                    code: 3,
+                    name: 'Colosse'
+                };
+            }
+    
+            return date;
+        }).then(date => {
             this.setState({
                 irl2rpError: null,
                 irl2rpMouvelian: `${date.day} du ${date.season.name}, ${date.year} AE.`,
@@ -53,8 +90,44 @@ class DateConverter extends Component {
         event.preventDefault();
         let { rpDateToConvertDay, rpDateToConvertSeason, rpDateToConvertYear } = this.state;
 
-        Axios.get(`https://api.nakasar.me/convertir/irl/${rpDateToConvertDay}/${rpDateToConvertSeason}/${rpDateToConvertYear}`).then(response => {
-            const date = new Date(response.data.day.date.irl);
+        Promise.resolve({
+            day: parseInt(rpDateToConvertDay),
+            season: parseInt(rpDateToConvertSeason),
+            year: parseInt(rpDateToConvertYear),
+        }).then(date => {
+            console.log(date.season);
+            const year = date.year + 687;
+            const bisex = year % 4 === 0;
+            const offset = bisex ? -1 : 0;
+    
+            let dayOfYear = 0;
+            switch (date.season) {
+                case 1:
+                    dayOfYear = date.day;
+                    break;
+                case 2:
+                    dayOfYear = 90 + date.day;
+                    break;
+                case 3:
+                    dayOfYear = 180 + offset + date.day;
+                    break;
+                case 4:
+                    dayOfYear = 270 + offset + date.day;
+                    break;
+                default:
+                    throw {
+                        message: 'Season must be between 1 and 4.',
+                        id: 'INVALID_SEASON',
+                        status: 400
+                    };
+            }
+    
+            const converted = new Date(year, 0, dayOfYear);
+    
+            return converted;
+        })
+        .then(converted => {
+            const date = new Date(converted);
             this.setState({
                 rp2irlError: null,
                 rp2irl: `${date.toLocaleDateString()}`,
